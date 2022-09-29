e2e82df5df5841ae5458612bfb50acf9bbed7529
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-09-29 21:18:32 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function one() {
+    console.log('1')
+}
+
+function two() {
+    setTimeout(function () {
+
+    }, 3000)
+}
+
+function three() {
+    console.log('3')
+}
+
+one()
+two()
+three()
